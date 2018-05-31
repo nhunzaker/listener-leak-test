0ccbccd39658c474ce2e84e73cf5ff047f060a04
hally9k <hally9k@gmail.com>
2018-05-31 17:15:56 +1200

    Refined further

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from 'component'
+
+class App extends React.Component {
+	constructor() {
+		super()
+
+		this.state = {
+			edit: false
+		}
+	}
+
+	handleClick = () => {
+		this.setState(() => ({ edit: !this.state.edit }))
+	}
+
+	render() {
+		return (
+			<div>
+				<button onClick={this.handleClick}>{this.state.edit ? 'view' : 'edit'}</button>
+				<br />
+
+				{this.state.edit ? <input /> : <p>view</p>}
+			</div>
+		)
+	}
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
