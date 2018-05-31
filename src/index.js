d4629396adc4788adf1d243e8c19c4ded95106e0
hally9k <hally9k@gmail.com>
2018-05-31 17:18:56 +1200

    Added instruction comment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+/*
+To reproduce event listener leak:
+
+1: Open dev tools and enter the performance tab.
+2: Start profiling.
+3: Repeat the folowing steps 3 or more times:
+  - click [edit] and click on the text input.
+  - click away from the input and click [view]
+  - click the garbage can at the top of the performance panel to trigger a Major GC.
+4: Stop profiling and observe the accumulation of event listeners and their respective nodes.
+*/
+
 class App extends React.Component {
 	constructor() {
 		super()
